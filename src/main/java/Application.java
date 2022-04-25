b24aeb4ad3eec5ece12cbb1f5846c1e268d57c94
Manh Nguyen The <manhnguyent1@nashtechglobal.com>
2022-04-25 22:23:34 +0700

    the commit 2nd - init source code

---
+++ src/main/java/Application.java
@@ -0,0 +1,10 @@
+public class Application {
+    public static void main(String[] args) {
+        Integer a = 10;
+        Long b = 100L;
+
+        Object x = b / a;
+        System.out.println(x);
+        System.out.println("Hello world!");
+    }
+}
